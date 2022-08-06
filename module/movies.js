e0971ecc08f93a0d0d2f30aaaf13f855518c9f84
Omar Qattam <omarqattam99@gmail.com>
2022-08-06 18:29:24 +0300

    city-explorer-api

--- module/movies.js
+++ module/movies.js
@@ -1,19 +1,26 @@
 
 const axios = require('axios');
 
+const moviesCache = {};
  async function movieData(req,res){
     let searchQuery = req.query.searchQuery;
   
   const keyMovie = process.env.MOVIE_API_KEY;
   
   const movieArr = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${keyMovie}&query=${searchQuery}`)
-  console.log(movieArr);
-  try {
-    const movieArray = movieArr.data.results.map(item => new Searchmovie(item));
-    res.status(200).send(movieArray);
-  } catch (error) {
-    handleError(error, res);
+
+  if(moviesCache[searchQuery] !== undefined){
+   res.status(200).json(moviesCache[searchQuery]);
+  }else {
+    try {
+      const movieArray = movieArr.data.results.map(item => new Searchmovie(item));
+      moviesCache[searchQuery] = movieArray;
+      res.status(200).send(movieArray);
+    } catch (error) {
+      handleError(error, res);
+    }
   }
+ 
   
   };
 
